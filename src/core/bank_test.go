60b65865491f6fe90f45fcceccdcc381eb17e820
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-10-24 23:15:55 +0300

    Implement deposit method

--- src/core/bank_test.go
+++ src/core/bank_test.go
@@ -16,3 +16,21 @@ func TestAccount(t *testing.T) {
 		t.Error("Can't create an Account object")
 	}
 }
+
+func TestDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "Micky",
+			Address: "Greece",
+			Phone:   "(30) 111 111",
+		},
+		Number:  2222,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+
+	if account.Balance != 10 {
+		t.Error("balance is not being updated!")
+	}
+}
